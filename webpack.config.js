2c91dad8697b6a4fc8b0ea2600c769aa2964b064
Romez19909 <Romez19900@gmail.com>
2019-02-13 14:29:05 +0300

    Setup build system

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path           = require('path');
+// const webpack        = require('webpack');
+const HtmlPlugin     = require('html-webpack-plugin');
+const MiniCssExtract = require('mini-css-extract-plugin');
+const del            = require('del');
+
+const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+
+const paths = {
+	src:  path.join(__dirname, 'src'),
+	dist: path.join(__dirname, 'dist')
+};
+
+del(paths.dist);
+
+module.exports = {
+	entry:        {
+		'index': path.join(paths.src, 'index', 'script.js'),
+		'blog':  path.join(paths.src, 'blog', 'script.js')
+	},
+	output:       {
+		path:     paths.dist,
+		filename: '[name].js'
+	},
+	devtool:      isDev ? 'cheap-inline-module-source-map' : false,
+	mode:         isDev ? 'development' : 'production',
+	watch:        isDev,
+	watchOptions: {
+		aggregateTimeout: 100
+	},
+	devServer:    {
+		compress: true,
+		overlay:  {
+			errors:   true,
+			warnings: true
+		},
+		open:     false
+	},
+	plugins:      [
+		/*
+		new webpack.SourceMapDevToolPlugin({
+			filename: '[file].map'
+		}),
+		*/
+		new HtmlPlugin({
+			filename: 'index.html',
+			chunks:   ['index'],
+			template: path.join(paths.src, 'index', 'index.pug')
+		}),
+		new HtmlPlugin({
+			filename: 'blog.html',
+			chunks:   ['blog'],
+			template: path.join(paths.src, 'blog', 'index.pug')
+		}),
+		new MiniCssExtract({
+			filename:      '[name].css',
+			chunkFilename: '[id].css'
+		})
+	],
+	module:       {
+		rules: [
+			{
+				test:    /\.pug$/,
+				exclude: /(node_modules|bower_components)/,
+				loader:  'pug-loader',
+				options: {
+					pretty: isDev
+				}
+			},
+			{
+				test:    /\.styl$/,
+				exclude: /(node_modules|bower_components)/,
+				use:     [MiniCssExtract.loader, 'css-loader', 'postcss-loader', 'stylus-loader']
+			},
+			{
+				test:    /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				loader:  'babel-loader'
+			}
+		]
+	}
+};
