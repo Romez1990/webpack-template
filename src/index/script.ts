83cbc1e98f6bd9a88c04bbc9fe991898cd1391a8
Romez1990 <Romez19900@gmail.com>
2019-04-07 22:26:06 +0300

    TypeScript support

---
+++ src/index/script.ts
@@ -0,0 +1,8 @@
+import './style.styl';
+import obj from '../components/hello.js';
+
+console.log(obj);
+
+let numb: number = 2;
+// numb             = false;
+console.log(numb);
--- webpack.config.js
+++ webpack.config.js
@@ -6,99 +6,104 @@ const MiniCssExtract = require('mini-css-extract-plugin');
 const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
 const paths = {
-	src:  path.join(__dirname, 'src'),
-	dist: path.join(__dirname, 'dist')
+    src:  path.join(__dirname, 'src'),
+    dist: path.join(__dirname, 'dist'),
 };
 
 module.exports = {
-	entry:        {
-		'index': path.join(paths.src, 'index', 'script.js'),
-		'blog':  path.join(paths.src, 'blog', 'script.js')
-	},
-	output:       {
-		path:     paths.dist,
-		filename: '[name].js'
-	},
-	devtool:      isDev ? 'cheap-inline-module-source-map' : false,
-	mode:         isDev ? 'development' : 'production',
-	watch:        isDev,
-	watchOptions: {
-		aggregateTimeout: 100
-	},
-	devServer:    {
-		compress: true,
-		overlay:  {
-			errors:   true,
-			warnings: true
-		},
-		open:     false
-	},
-	plugins:      [
-		/*
-		new webpack.SourceMapDevToolPlugin({
-			filename: '[file].map'
-		}),
-		*/
-		new HtmlPlugin({
-			filename: 'index.html',
-			chunks:   ['index'],
-			template: path.join(paths.src, 'index', 'index.pug')
-		}),
-		new HtmlPlugin({
-			filename: 'blog.html',
-			chunks:   ['blog'],
-			template: path.join(paths.src, 'blog', 'index.pug')
-		}),
-		new MiniCssExtract({
-			filename:      '[name].css',
-			chunkFilename: '[id].css'
-		})
-	],
-	module:       {
-		rules: [
-			{
-				test:    /\.pug$/,
-				exclude: /(node_modules|bower_components)/,
-				loader:  'pug-loader',
-				options: {
-					pretty: isDev
-				}
-			},
-			{
-				test:    /\.styl$/,
-				exclude: /(node_modules|bower_components)/,
-				use:     [
-					{
-						loader: MiniCssExtract.loader/*,
-						options: {
-							sourceMap: isDev
-						}*/
-					},
-					{
-						loader:  'css-loader',
-						options: {
-							sourceMap: isDev
-						}
-					},
-					{
-						loader: 'stylus-loader'/*,
-						options: {
-							sourceMap: isDev
-						}*/
-					}
-				]
-			},
-			{
-				test:    /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				use:     {
-					loader:  'babel-loader',//?optional[]=runtime&stage=0',
-					options: {
-						presets: ['@babel/preset-env'],
-						plugins: ['@babel/plugin-transform-runtime']
-					}
-				}
-			}
-		]
-	}
+    entry:        {
+        'index': path.join(paths.src, 'index', 'script.ts'),
+        'blog':  path.join(paths.src, 'blog', 'script.ts'),
+    },
+    output:       {
+        path:     paths.dist,
+        filename: '[name].js',
+    },
+    devtool:      isDev ? 'cheap-inline-module-source-map' : false,
+    mode:         isDev ? 'development' : 'production',
+    watch:        isDev,
+    watchOptions: {
+        aggregateTimeout: 100,
+    },
+    devServer:    {
+        compress: true,
+        overlay:  {
+            errors:   true,
+            warnings: true,
+        },
+        open:     false,
+    },
+    plugins:      [
+        /*
+         new webpack.SourceMapDevToolPlugin({
+         filename: '[file].map'
+         }),
+         */
+        new HtmlPlugin({
+            filename: 'index.html',
+            chunks:   ['index'],
+            template: path.join(paths.src, 'index', 'index.pug'),
+        }),
+        new HtmlPlugin({
+            filename: 'blog.html',
+            chunks:   ['blog'],
+            template: path.join(paths.src, 'blog', 'index.pug'),
+        }),
+        new MiniCssExtract({
+            filename:      '[name].css',
+            chunkFilename: '[id].css',
+        }),
+    ],
+    module:       {
+        rules: [
+            {
+                test:    /\.pug$/,
+                exclude: /(node_modules|bower_components)/,
+                loader:  'pug-loader',
+                options: {
+                    pretty: isDev,
+                },
+            },
+            {
+                test:    /\.styl$/,
+                exclude: /(node_modules|bower_components)/,
+                use:     [
+                    {
+                        loader: MiniCssExtract.loader,/*,
+                         options: {
+                         sourceMap: isDev
+                         }*/
+                    },
+                    {
+                        loader:  'css-loader',
+                        options: {
+                            sourceMap: isDev,
+                        },
+                    },
+                    {
+                        loader: 'stylus-loader',/*,
+                         options: {
+                         sourceMap: isDev
+                         }*/
+                    },
+                ],
+            },
+            {
+                test:    /\.tsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                use:     [
+                    {
+                        loader:  'babel-loader',//?optional[]=runtime&stage=0',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                            plugins: ['@babel/plugin-transform-runtime'],
+                        },
+                    },
+                    {
+                        loader: 'ts-loader',
+                    },
+                ],
+            },
+        ],
+    },
 };
